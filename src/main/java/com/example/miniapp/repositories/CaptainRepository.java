8e0d205c86c563d59b6a0ed44e17c968a108d485
Omar Hesham <omar.hesham-youssef@outlook.com>
2025-04-13 17:52:57 +0200

    Convert repositories to interfaces Add annotations to all repositories, services, and controllers

--- src/main/java/com/example/miniapp/repositories/CaptainRepository.java
+++ src/main/java/com/example/miniapp/repositories/CaptainRepository.java
@@ -2,9 +2,11 @@ package com.example.miniapp.repositories;
 
 import com.example.miniapp.models.Captain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CaptainRepository extends JpaRepository<Captain, Long> {
 
     List<Captain> findByAvgRatingScoreGreaterThanEqual(double ratingThreshold);
--- src/main/java/com/example/miniapp/repositories/TripRepository.java
+++ src/main/java/com/example/miniapp/repositories/TripRepository.java
@@ -1,4 +1,7 @@
 package com.example.miniapp.repositories;
 
-public class TripRepository {
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TripRepository {
 }
