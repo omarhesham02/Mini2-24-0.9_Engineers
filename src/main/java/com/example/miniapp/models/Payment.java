bed70088dad89d33b5fc62b13ba2e48794d41a31
Hosain Ghoraba <hussain.ghoraba@gmail.com>
2025-04-19 11:50:08 +0200

    add constructors to payment model

--- src/main/java/com/example/miniapp/models/Payment.java
+++ src/main/java/com/example/miniapp/models/Payment.java
@@ -20,4 +20,23 @@ public class Payment {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "trip_id", nullable = false)
     private Trip trip;
+
+    // Default constructor
+    public Payment() {
+    }
+
+    // full constructor
+    public Payment(double amount, String paymentMethod, boolean paymentStatus, Trip trip) {
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.paymentStatus = paymentStatus;
+        this.trip = trip;
+    }
+
+    // TODO: make sure, which fields to include ? (test cases may help)
+    // partial constructor
+    public Payment(double amount, String paymentMethod) {
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+    }
 }
--- src/main/java/com/example/miniapp/services/PaymentService.java
+++ src/main/java/com/example/miniapp/services/PaymentService.java
@@ -2,6 +2,15 @@ package com.example.miniapp.services;
 
 import org.springframework.stereotype.Service;
 
+import com.example.miniapp.models.Payment;
+import com.example.miniapp.repositories.PaymentRepository;
+
 @Service
 public class PaymentService {
+
+    PaymentRepository paymentRepository;
+
+    public PaymentService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
 }
