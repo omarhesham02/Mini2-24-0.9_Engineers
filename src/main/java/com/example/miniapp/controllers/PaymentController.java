c3ca7d9e7c72beb492af429e202d3eb0caa66db7
Hosain Ghoraba <hussain.ghoraba@gmail.com>
2025-04-19 12:25:23 +0200

    implement payment controller

--- src/main/java/com/example/miniapp/controllers/PaymentController.java
+++ src/main/java/com/example/miniapp/controllers/PaymentController.java
@@ -1,9 +1,64 @@
 package com.example.miniapp.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.miniapp.models.Payment;
+import com.example.miniapp.services.PaymentService;
 
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/addPayment")
+    public Payment addPayment(@RequestBody Payment payment) {
+        return paymentService.addPayment(payment);
+    }
+
+    @GetMapping("/allPayments")
+    public List<Payment> getAllPayments() {
+        return paymentService.getAllPayments();
+    }
+
+    @GetMapping("/{id}")
+    public Payment getPaymentById(@PathVariable Long id) {
+        return paymentService.getPaymentById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Payment updatePayment(@PathVariable Long id, @RequestBody Payment payment) {
+        return paymentService.updatePayment(id, payment);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deletePayment(@PathVariable Long id) {
+        paymentService.deletePayment(id);
+        return "Payment with id " + id + " deleted successfully";
+    }
+
+    @GetMapping("/findByTripId")
+    public List<Payment> findPaymentsByTripId(@RequestParam Long tripId) {
+        return paymentService.findPaymentsByTripId(tripId);
+    }
+
+    @GetMapping("/findByAmountThreshold")
+    public List<Payment> findPaymentsWithAmountGreaterThan(@RequestParam Double threshold) {
+        return paymentService.findByAmountThreshold(threshold);
+    }
 }
