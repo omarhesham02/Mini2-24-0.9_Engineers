ce5e686a699669ffb35e865d89ccb398cb4792af
OmarT123 <94041039+OmarT123@users.noreply.github.com>
2025-04-22 17:57:55 +0200

    trip complete

--- src/main/java/com/example/miniapp/controllers/TripController.java
+++ src/main/java/com/example/miniapp/controllers/TripController.java
@@ -1,9 +1,61 @@
 package com.example.miniapp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.miniapp.models.Trip;
+import com.example.miniapp.services.TripService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/trip")
 public class TripController {
+    private final TripService tripService;
+
+    @Autowired
+    public TripController(TripService tripService) {
+        this.tripService = tripService;
+    }
+
+    @PostMapping("/addTrip")
+    public Trip addTrip(@RequestBody Trip trip) {
+        return tripService.addTrip(trip);
+    }
+
+    @GetMapping("/allTrips")
+    public List<Trip> getAllTrips() {
+        return tripService.getAllTrips();
+    }
+
+    @GetMapping("/{id}")
+    public Trip getTripById(@PathVariable Long id) {
+        return tripService.getTripById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Trip updateTrip(@PathVariable Long id, @RequestBody Trip trip) {
+        return tripService.updateTrip(id, trip);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteTrip(@PathVariable Long id) {
+        tripService.deleteTrip(id);
+        return "Trip deleted successfully";
+    }
+
+
+
+    @GetMapping("/findByDateRange")
+    public List<Trip> findTripsWithinDateRange(@RequestParam LocalDateTime startDate,
+                                               @RequestParam LocalDateTime endDate) {
+        return tripService.findTripsWithinDateRange(startDate, endDate);
+    }
+
+    @GetMapping("/findByCaptainId")
+    public List<Trip> findTripsByCaptainId(@RequestParam Long captainId) {
+        return tripService.findTripsByCaptainId(captainId);
+    }
 }
