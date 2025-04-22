970161d9b0af5fbce648ff0550d95f6818a55b40
MoTammaa <mohamed.abotammaa@student.guc.edu.eg>
2025-04-23 01:30:57 +0200

    added required CustomerService functions

--- src/main/java/com/example/miniapp/services/CustomerService.java
+++ src/main/java/com/example/miniapp/services/CustomerService.java
@@ -1,17 +1,45 @@
 package com.example.miniapp.services;
 
 import com.example.miniapp.models.Customer;
+import com.example.miniapp.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
     public Customer addCustomer(Customer customer) {
-        return customer;
+        return customerRepository.save(customer);
     }
 
     public List<Customer> getAllCustomers() {
-        return List.of();
+        return customerRepository.findAll();
     }
+
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found with id: " + id));
+    }
+
+    public Customer updateCustomer(Long id, Customer customer) {
+        customerRepository.updateCustomer(customer);
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found with id: " + id));
+    }
+
+    public void deleteCustomer(Long id) { customerRepository.deleteById(id); }
+
+    public List<Customer> findCustomersByEmailDomain(String domain) { return customerRepository.findCustomersByEmailDomain(domain); }
+
+    public List<Customer> findCustomersByPhonePrefix(String prefix) { return customerRepository.findCustomersByPhonePrefix(prefix);}
+
 }
